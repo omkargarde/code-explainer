e7dfe200f60cf34e7af72fb9c1af1b8fe7bdc4b2
omkar garde <omyagarde@gmail.com>
2025-10-26 17:18:04 +0530

    fix for env import

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,4 +1,4 @@
-import { environmentVariables } from '@/env'
+import { environmentVariables } from '@/Env'
 import { drizzle } from 'drizzle-orm/neon-serverless'
 
 export const db = drizzle(environmentVariables.DATABASE_URL)
