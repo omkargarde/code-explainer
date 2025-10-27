6cd406a229ae49a7a9c5057ddf7651ffa6e6dd82
omkar garde <omyagarde@gmail.com>
2025-10-27 09:38:42 +0530

    refactored code component to make it simpler

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -6,18 +6,25 @@ export default function User() {
     // TODO: redirect to error page
   }
   if (isPending) {
-    return <h1>Loading</h1>
+    return (
+      <button
+        className="disabled cursor-not-allowed items-center justify-center rounded-lg bg-blue-500 px-4 py-3 align-middle font-semibold text-gray-200 transition-opacity hover:bg-blue-400 hover:shadow-lg"
+        onClick={() => useSignOut()}
+      >
+        Loading
+      </button>
+    )
   }
   return session?.user ? (
     <button
-      className="cursor-pointer items-center justify-center rounded-lg bg-blue-500 px-4 py-3 align-middle font-semibold text-gray-200 transition-opacity hover:opacity-90 hover:shadow-lg"
+      className="cursor-pointer items-center justify-center rounded-lg bg-blue-500 px-4 py-3 align-middle font-semibold text-gray-200 transition-opacity hover:bg-blue-600 hover:shadow-lg"
       onClick={() => useSignOut()}
     >
       Sign out
     </button>
   ) : (
     <button
-      className="cursor-pointer items-center justify-center rounded-lg bg-blue-500 px-4 py-3 align-middle font-semibold text-gray-200 transition-opacity hover:opacity-90 hover:shadow-lg"
+      className="cursor-pointer items-center justify-center rounded-lg bg-blue-500 px-4 py-3 align-middle font-semibold text-gray-200 transition-opacity hover:bg-blue-600 hover:shadow-lg"
       onClick={() => useSignIn()}
     >
       Sign in
