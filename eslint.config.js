2ef9a7f387b2a8a688e48c9d7b4410d2569f06e9
omkar garde <omyagarde@gmail.com>
2025-11-19 19:40:03 +0530

    added oxlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,19 @@
 //  @ts-check
 
-import { tanstackConfig } from '@tanstack/eslint-config'
-import pluginQuery from '@tanstack/eslint-plugin-query'
+import { tanstackConfig } from "@tanstack/eslint-config";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import oxlint from "eslint-plugin-oxlint";
 
 export default [
-  ...pluginQuery.configs['flat/recommended'],
+  ...pluginQuery.configs["flat/recommended"],
   ...tanstackConfig,
   {
     languageOptions: {
       parserOptions: {
-        project: './tsconfig.json', // ✅ correct relative path
+        project: "./tsconfig.json", // ✅ correct relative path
         tsconfigRootDir: import.meta.dirname, // ✅ works in flat config
       },
     },
   },
-]
+  ...oxlint.configs["flat/recommended"], // oxlint should be the last one
+];
