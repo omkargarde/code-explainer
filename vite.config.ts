c22da9182326ecd56296bb9713f08686d61ade03
omkar garde <omyagarde@gmail.com>
2025-12-08 13:47:35 +0530

    netlify deployment traget

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 const config = defineConfig({
   plugins: [
@@ -13,8 +13,8 @@ const config = defineConfig({
       projects: ["./tsconfig.json"],
     }),
     tailwindcss(),
+    netlify(),
     tanstackStart(),
-    nitro(),
     viteReact(),
   ],
   resolve: {
